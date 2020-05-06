2c4dea71ed68d07b094c8f9e9a16ac05d7477529
Amana Ammishaddai <amana.ammi@gmail.com>
2020-05-06 22:35:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-   "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   else if phrase == phrase.upcase
-   "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP SONNY!"
   end
